64f0aaf82af606100ef879ba1da2c23b1676d4bd
Patrick Sachs <kontakt@patrick-sachs.de>
2022-02-09 22:26:12 +0100

    Added: Warning interface

---
+++ SahneeBotController/IWarning.cs
@@ -0,0 +1,14 @@
+﻿namespace SahneeBotController;
+
+/// <summary>
+/// A warning for the Sahnee bot.
+/// </summary>
+public interface IWarning
+{
+    DateTime Time { get; set; }
+    ulong GuildId { get; set; }
+    ulong From { get; set; }
+    ulong To { get; set; }
+    string Reason { get; set; }
+    ulong Number { get; set; }
+}
