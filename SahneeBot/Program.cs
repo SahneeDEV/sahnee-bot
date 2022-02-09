f9aa3d3df18ba0baead716e9ffff17b5a53911cd
Patrick Sachs <kontakt@patrick-sachs.de>
2022-02-09 22:00:09 +0100

    Added: User secrets

--- SahneeBot/Program.cs
+++ SahneeBot/Program.cs
@@ -1,3 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.Extensions.Configuration;
 
 Console.WriteLine("Hello, World!");
+
+var config = new ConfigurationBuilder()
+    .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+    .AddJsonFile("appsettings.json")
+    .AddUserSecrets<Program>()
+    .Build();
+    
+Console.WriteLine("Secret config is " + config["Test:Key"]);
