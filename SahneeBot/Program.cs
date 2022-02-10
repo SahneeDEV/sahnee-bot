ebe7cb1d414cd1347c1ac3a2aef30bad9f64068b
Patrick Sachs <kontakt@patrick-sachs.de>
2022-02-10 22:08:39 +0100

    Changed: Simpler Program.cs

--- SahneeBot/Program.cs
+++ SahneeBot/Program.cs
@@ -4,68 +4,39 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using SahneeBotController;
 using SahneeBotModel;
-using System;
 
-namespace SahneeBot
+// Create a configuration
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+    .AddJsonFile("appsettings.json")
+    .AddUserSecrets<Program>()
+    .Build();
+
+// Register DI Services
+var services = new ServiceCollection();
+
+// Add services
+services.AddLogging();
+services.AddDbContext<SahneeBotModelContext>(options =>
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-            new Program().MainTask().GetAwaiter().GetResult();
-        }
+    options.UseNpgsql()
+        .LogTo(Console.WriteLine, LogLevel.Information)
+        .EnableSensitiveDataLogging()
+        .EnableDetailedErrors();
+});
+services.AddSingleton<IConfiguration>(provider => configuration);
 
-        /// <summary>
-        /// Main Task. Initiates the Discord Bot
-        /// </summary>
-        public async Task MainTask()
-        {
-            //Variables
-            IServiceProvider _serviceProvider;
+// Build to service provider
+var serviceProvider = services.BuildServiceProvider(true);
 
-            //Create a configuration
-            var configuration = new ConfigurationBuilder()
-                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
-                .AddJsonFile("appsettings.json")
-                .AddUserSecrets<Program>()
-                .Build();
+// Define Discord Client
+var discordConfig = new DiscordSocketConfig()
+{
+    GatewayIntents = GatewayIntents.All,
+    AlwaysDownloadUsers = true
+};
+var discordSocketClient = new DiscordSocketClient(discordConfig);
 
-            //Register DI Services
-            ServiceCollection? services = new ServiceCollection();
-
-            //add services
-            services.AddLogging();
-            services.AddDbContext<SahneeBotModelContext>(options =>
-            {
-                options.UseNpgsql()
-                    .LogTo(Console.WriteLine, LogLevel.Information)
-                    .EnableSensitiveDataLogging()
-                    .EnableDetailedErrors();
-            });
-            services.AddSingleton<IConfiguration>(provider => configuration);
-
-
-            //build to service provider
-            _serviceProvider = services.BuildServiceProvider(true);
-            
-            
-
-            //Define Discord Client
-            DiscordSocketClient discordSocketClient;
-
-            DiscordSocketConfig discordConfig = new DiscordSocketConfig()
-            {
-                GatewayIntents = GatewayIntents.All,
-                AlwaysDownloadUsers = true
-            };
-
-            discordSocketClient = new DiscordSocketClient(discordConfig);
-            
-            // Block this task until the program is closed. <--- From the Discord.Net Guide
-            await Task.Delay(-1);
-        }
-    }
-}
+// Block this task until the program is closed. <--- From the Discord.Net Guide
+await Task.Delay(-1);
