5abc3cc8ceccfbf60b84a8687d1ea22eb2643c7b
DaNeubi <lukas.neubauer@sahnee.de>
2022-02-10 21:03:54 +0100

    added logging structure
    + changed from .net 6 program.cs to .net 5 program.cs layout

---
+++ SahneeBot/Logging/Logger.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace SahneeBot.Logging;
+
+public class Logger
+{
+    //Variables
+    private LogLevel _logLevel = LogLevel.Warning; //Default logging level
+
+    
+    /// <summary>
+    /// Creates a new instance of the logger
+    /// </summary>
+    /// <param name="configuration"></param>
+    public Logger(IConfiguration configuration)
+    {
+        try
+        {
+            //check if a loglevel has been set
+            if (!String.IsNullOrWhiteSpace(configuration["Logging:LogLevel:Default"]))
+            {
+                _logLevel = Enum.Parse<LogLevel>(configuration["Logging:LogLevel:Default"]);
+            }
+        }
+        catch
+        {
+            Console.WriteLine("Logging configuration could not be loaded! Reverting to default log level!");
+        }
+    }
+
+    /// <summary>
+    /// Logging with the classname as addition for further easy progressing
+    /// </summary>
+    /// <param name="message"></param>
+    /// <param name="logLevel"></param>
+    /// <param name="className"></param>
+    /// <returns></returns>
+    public Task Log(string message, LogLevel logLevel, string className)
+    {
+        try
+        {
+            
+            return Task.CompletedTask;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Could not Log!" + e);
+            return Task.CompletedTask;
+        }
+    }
+
+    /// <summary>
+    /// Defines the log suffix that will be appended to every log message
+    /// </summary>
+    /// <returns></returns>
+    private string LogSuffix()
+    {
+        return "[" + DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss") + "]: ";
+    }
+
+    /// <summary>
+    /// Logs to console
+    /// </summary>
+    /// <param name="message"></param>
+    /// <param name="logLevel"></param>
+    /// <returns></returns>
+    private Task SendLogToConsole(string message, LogLevel logLevel)
+    {
+        //check if the loglevel of the log message is equal or higher than the current loglevel
+        
+        return Task.CompletedTask;
+    }
+    
+}
--- SahneeBot/Program.cs
+++ SahneeBot/Program.cs
@@ -1,46 +1,70 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Discord;
+using Discord.WebSocket;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using SahneeBotController;
 using SahneeBotModel;
+using System;
 
-Console.WriteLine("Hello, World!");
-
-var config = new ConfigurationBuilder()
-    .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
-    .AddJsonFile("appsettings.json")
-    .AddUserSecrets<Program>()
-    .Build();
-
-Console.WriteLine("Secret config is " + config["Test:Key"]);
-
-//Variables
-IServiceProvider _serviceProvider;
-
-//Create a configuration
-IConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
-configurationBuilder.AddJsonFile("appsettings.json");
-IConfiguration configuration = configurationBuilder.Build();
-
-//Register DI Services
-ServiceCollection? services = new ServiceCollection();
-
-//add services
-services.AddDbContext<SahneeBotModelContext>(options =>
+namespace SahneeBot
 {
-    options.UseNpgsql()
-        .LogTo(Console.WriteLine, LogLevel.Information)
-        .EnableSensitiveDataLogging()
-        .EnableDetailedErrors();
-});
-services.AddSingleton<IConfiguration>(provider => configuration);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            new Program().MainTask().GetAwaiter().GetResult();
+        }
+
+        /// <summary>
+        /// Main Task. Initiates the Discord Bot
+        /// </summary>
+        public async Task MainTask()
+        {
+            //Variables
+            IServiceProvider _serviceProvider;
+
+            //Create a configuration
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .AddUserSecrets<Program>()
+                .Build();
+
+            //Register DI Services
+            ServiceCollection? services = new ServiceCollection();
+
+            //add services
+            services.AddDbContext<SahneeBotModelContext>(options =>
+            {
+                options.UseNpgsql()
+                    .LogTo(Console.WriteLine, LogLevel.Information)
+                    .EnableSensitiveDataLogging()
+                    .EnableDetailedErrors();
+            });
+            services.AddSingleton<IConfiguration>(provider => configuration);
 
 
-//build to service provider
-_serviceProvider = services.BuildServiceProvider(true);
+            //build to service provider
+            _serviceProvider = services.BuildServiceProvider(true);
+            
+            
 
+            //Define Discord Client
+            DiscordSocketClient discordSocketClient;
 
+            DiscordSocketConfig discordConfig = new DiscordSocketConfig()
+            {
+                GatewayIntents = GatewayIntents.All,
+                AlwaysDownloadUsers = true
+            };
 
-Class1 class1 = new Class1(_serviceProvider);
-
+            discordSocketClient = new DiscordSocketClient(discordConfig);
+            
+            // Block this task until the program is closed. <--- From the Discord.Net Guide
+            await Task.Delay(-1);
+        }
+    }
+}
