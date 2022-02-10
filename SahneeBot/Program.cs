15474baf54795e22c3ac0f8335b01b675577c904
DaNeubi <lukas.neubauer@sahnee.de>
2022-02-10 17:12:48 +0100

    added basic dependency injection
    with a sample

--- SahneeBot/Program.cs
+++ SahneeBot/Program.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using SahneeBotController;
+using SahneeBotModel;
 
 Console.WriteLine("Hello, World!");
 
@@ -7,5 +12,35 @@ var config = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json")
     .AddUserSecrets<Program>()
     .Build();
-    
+
 Console.WriteLine("Secret config is " + config["Test:Key"]);
+
+//Variables
+IServiceProvider _serviceProvider;
+
+//Create a configuration
+IConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+configurationBuilder.AddJsonFile("appsettings.json");
+IConfiguration configuration = configurationBuilder.Build();
+
+//Register DI Services
+ServiceCollection? services = new ServiceCollection();
+
+//add services
+services.AddDbContext<SahneeBotModelContext>(options =>
+{
+    options.UseNpgsql()
+        .LogTo(Console.WriteLine, LogLevel.Information)
+        .EnableSensitiveDataLogging()
+        .EnableDetailedErrors();
+});
+services.AddSingleton<IConfiguration>(provider => configuration);
+
+
+//build to service provider
+_serviceProvider = services.BuildServiceProvider(true);
+
+
+
+Class1 class1 = new Class1(_serviceProvider);
+
--- SahneeBotController/Class1.cs
+++ SahneeBotController/Class1.cs
@@ -1,5 +1,21 @@
-﻿namespace SahneeBotController;
+﻿using Microsoft.Extensions.DependencyInjection;
+using SahneeBotModel;
+
+namespace SahneeBotController;
 
 public class Class1
 {
+    public Class1(IServiceProvider services)
+    {
+        using var serviceScopoe = services.CreateScope();
+        var provider = serviceScopoe.ServiceProvider;
+        
+        test(provider.GetRequiredService<SahneeBotModelContext>());
+    }
+
+    private void test(SahneeBotModelContext sahneeBotModelContext)
+    {
+        Console.Write("Dependency Injection!");
+    }
+    
 }
