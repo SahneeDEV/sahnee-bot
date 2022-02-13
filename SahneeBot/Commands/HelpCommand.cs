535bc6165b94cf44a3d01015a648893ebe335325
JakobHeubl <jakob.heubl@sahnee.dev>
2022-02-13 23:11:09 +0100

    Added: Help Command

---
+++ SahneeBot/Commands/HelpCommand.cs
@@ -0,0 +1,37 @@
+﻿using Discord;
+using Discord.Interactions;
+using SahneeBot.Formatter;
+
+namespace SahneeBot.Commands;
+
+/// <summary>
+/// This command is used to give additional information about the bot.
+/// </summary>
+public class HelpCommand: InteractionModuleBase<IInteractionContext>
+{
+   
+    private readonly HelpDiscordFormatter _discordFormatter;
+
+    public HelpCommand(HelpDiscordFormatter discordFormatter)
+    {
+        _discordFormatter = discordFormatter;
+    }
+    
+    /// <summary>
+    /// The actual help command.
+    /// </summary>
+    [SlashCommand("help", "Gives a details about the bot")]
+    public async Task Help()
+    {
+        
+        try
+        {
+            await _discordFormatter.FormatAndSend(new HelpDiscordFormatter.Args(), RespondAsync);
+        }
+        catch (Exception e)
+        {
+           // _logger.LogWarning(EventIds.Command, e, "Failed to send warning message: {Warning}", 
+           //     warning);
+        }
+    }
+}
---
+++ SahneeBot/Formatter/HelpDiscordFormatter.cs
@@ -0,0 +1,51 @@
+﻿using Discord;
+
+namespace SahneeBot.Formatter;
+
+public class HelpDiscordFormatter: IDiscordFormatter<HelpDiscordFormatter.Args>
+{
+    /// <summary>
+    /// Arguments for the generation of the format of the help command
+    /// </summary>
+    public struct Args
+    {
+        /// <summary>
+        /// The website of the sahnee bot
+        /// </summary>
+        public readonly string Website = "https://sahnee.dev/en/project/sahnee-bot/";
+        /// <summary>
+        /// The github repo address of the sahnee bot
+        /// </summary>
+        public readonly string Github = "https://github.com/Sahnee-DE/sahnee-bot";
+    }
+    
+    private readonly DefaultFormatArguments _defaultFormatArguments;
+
+    public HelpDiscordFormatter(DefaultFormatArguments defaultFormatArguments)
+    {
+        _defaultFormatArguments = defaultFormatArguments;
+    }
+
+    public Task<DiscordFormat> Format(Args arg)
+    {
+        var embed = _defaultFormatArguments.GetEmbed();
+        embed.Title = "More details about this bot:";
+        embed.Fields = new List<EmbedFieldBuilder>()
+        {
+            new EmbedFieldBuilder()
+            {
+                Name = "Website of the bot",
+                Value = arg.Website,
+                IsInline = true
+            },
+            new EmbedFieldBuilder()
+            {
+                Name = "GitHub Repository of the bot",
+                Value = arg.Github,
+                IsInline = true
+            }
+        };
+        
+        return Task.FromResult(new DiscordFormat(embed.Build()));
+    }
+}
