b999512c0a9962744051d3957b2229a0a386efc5
DaNeubi <lukas.neubauer@sahnee.de>
2022-02-13 21:59:36 +0100

    added embed for warnings + customization of the icon and the url in the icon

--- SahneeBot/Formatter/WarningDiscordFormatter.cs
+++ SahneeBot/Formatter/WarningDiscordFormatter.cs
@@ -1,4 +1,6 @@
-﻿using Discord.WebSocket;
+﻿using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
 using SahneeBotModel.Contract;
 
 namespace SahneeBot.Formatter;
@@ -6,18 +8,76 @@ namespace SahneeBot.Formatter;
 public class WarningDiscordFormatter: IDiscordFormatter<IWarning>
 {
     private readonly DiscordSocketClient _bot;
+    private readonly IConfiguration _configuration;
 
-    public WarningDiscordFormatter(DiscordSocketClient bot)
+    public WarningDiscordFormatter(DiscordSocketClient bot, IConfiguration configuration)
     {
         _bot = bot;
+        _configuration = configuration;
     }
     
     public Task<DiscordFormat> Format(IWarning arg)
     {
         var guild = _bot.GetGuild(arg.GuildId);
+        var user = _bot.GetUser(arg.UserId);
         return Task.FromResult(new DiscordFormat
             {
-                Text = $":thumbsdown: [{guild.Name}] <@{arg.UserId}> has been warned by <@{arg.IssuerUserId}> at {arg.Time}. This is warning #{arg.Number}. (Reason: {arg.Reason})"
+                Embed = new EmbedBuilder()
+                {
+                    Author = new EmbedAuthorBuilder()
+                    {
+                        Name = "SahneeBot",
+                        Url = !string.IsNullOrWhiteSpace(_configuration["Url"])
+                        ? _configuration["Url"]
+                        : "https://sahnee.dev/en/project/sahnee-bot/",
+                        IconUrl = !string.IsNullOrWhiteSpace(_configuration["BotSettings:IconUrl"])
+                            ? _configuration["IconUrl"] 
+                            : "https://sahnee.dev/wp-content/uploads/2020/04/sahnee-bot-150x150.png"
+                    },
+                    Color = new Color(243,9,131),
+                    Title = $":thumbsdown: {user.Username} has been warned",
+                    Fields = new List<EmbedFieldBuilder>()
+                    {
+                        new EmbedFieldBuilder()
+                        {
+                            Name = "Warned",
+                            Value = $"<@{arg.UserId}>",
+                            IsInline = true
+                        },
+                        new EmbedFieldBuilder()
+                        {
+                            Name = "Warned by",
+                            Value = $"<@{arg.IssuerUserId}>",
+                            IsInline = true
+                        },
+                        new EmbedFieldBuilder()
+                        {
+                            Name = "Warning Number",
+                            Value = $"{arg.Number}",
+                            IsInline = true
+                        },
+                        new EmbedFieldBuilder()
+                        {
+                            Name = "In Server",
+                            Value = $"{guild.Name}",
+                            IsInline = true
+                        },
+                        new EmbedFieldBuilder()
+                        {
+                            Name = "Message",
+                            Value = arg.Reason,
+                            IsInline = false
+                        }
+                    },
+                    Timestamp = arg.Time,
+                    Footer = new EmbedFooterBuilder()
+                    {
+                        Text = "proudly presented by sahnee.dev",
+                        IconUrl = !string.IsNullOrWhiteSpace(_configuration["BotSettings:IconUrl"])
+                            ? _configuration["IconUrl"] 
+                            : "https://sahnee.dev/wp-content/uploads/2020/04/sahnee-bot-150x150.png"
+                    }
+                }.Build()
             });
     }
 }
