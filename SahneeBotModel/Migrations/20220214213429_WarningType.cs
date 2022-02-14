19c169da66213ead93bb17c318b0b1467c0ece65
Patrick Sachs <kontakt@patrick-sachs.de>
2022-02-14 22:52:47 +0100

    Added: Warning type migration

---
+++ SahneeBotModel/Migrations/20220214213429_WarningType.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SahneeBotModel.Migrations
+{
+    public partial class WarningType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Type",
+                table: "Warnings",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Warnings");
+        }
+    }
+}
