113b09a91a7cd80a85294c588154a43c23954641
DaNeubi <lukas.neubauer@sahnee.de>
2022-03-08 22:05:08 +0100

    added: changelog information for all guild for migration on friday 11.03.2022

--- sahnee-bot/Util/StaticBot.cs
+++ sahnee-bot/Util/StaticBot.cs
@@ -5,7 +5,7 @@ namespace sahnee_bot.Util
     public static class StaticBot
     {
         private static IDiscordClient sahneeBot;
-        private static string BotVersion = "Version 0.9.95:";
+        private static string BotVersion = "Version 0.9.96:";
 
         /// <summary>
         /// Sets the current bot
